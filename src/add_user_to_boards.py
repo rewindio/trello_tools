9cf779feed46f14a30d09bdb73eb5fdf8ed00b89
Evan Salter <evan.salter@rewind.io>
2023-08-14 11:51:59 -0600

    Parse tool name, run script

---
+++ src/add_user_to_boards.py
@@ -0,0 +1,8 @@
+from trello import TrelloClient
+
+class AddUserToBoards():
+  def __init__(self):
+    pass
+
+  def run(self):
+    print("Running add-user-to-boards script")
--- src/cli.py
+++ src/cli.py
@@ -1,7 +1,16 @@
-from trello import TrelloClient
+from add_user_to_boards import AddUserToBoards
+import argparse
 
 def main():
-  print("Hello, World!")
+  parser = argparse.ArgumentParser(description='Blah blah')
+  parser.add_argument('tool_name',
+                      help='the name of the tool you want to run',
+                      choices=['add-user-to-boards'])
+
+  args = parser.parse_args()
+
+  if args.tool_name == 'add-user-to-boards':
+    AddUserToBoards().run()
 
 if __name__ == "__main__":
   main()
